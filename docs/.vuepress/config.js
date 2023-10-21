5d72e8b7dda98fc5fda9550d6e8cf7d9de598eba
8ecomeDreamer <1412415464@qq.com>
2023-10-21 11:19:32 +0800

    更新

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,9 +4,11 @@ module.exports = {
   description: "属于coderWorld的知识体系库",
   base: "/",
   lastUpdated: "Last Updated", // string | boolean
+  // base: "./",
   themeConfig: {
     nav: [
-      { text: "技术笔记", link: "/noteBook/" },
+      { text: "首页", link: "/" },
+      // { text: "技术笔记", link: "/" },
       { text: "生活不止代码", link: "/withoutCode/" },
       {
         text: "关于码外生活",
@@ -20,99 +22,6 @@ module.exports = {
       },
     ],
     sidebar: {
-      "/noteBook/": [
-        {
-          title: "站点介绍",
-          path: "/noteBook/",
-          collapsable: true, // 不折叠
-          children: [],
-        },
-        {
-          title: "设计",
-          path: "/noteBook/design/",
-          collapsable: false, // 不折叠
-          sidebarDepth: 5,
-          children: [],
-        },
-        {
-          title: "前端",
-          path: "/noteBook/frontEnd/",
-          collapsable: false, // 不折叠
-          // children: [{ title: "前端", path: "/noteBook/frontEnd/index.md" }],
-        },
-        {
-          title: "后端",
-          path: "/noteBook/backEnd/",
-          collapsable: false, // 不折叠
-          // 多级嵌套看这里的目录结构
-          children: [
-            {
-              title: "java",
-              path: "/noteBook/backEnd/java/",
-              children: [
-                { title: "api速记", path: "/noteBook/backEnd/java/api/" },
-              ],
-            },
-          ],
-        },
-        {
-          title: "sql",
-          path: "/noteBook/sql/",
-          collapsable: false, // 不折叠
-          // children: [{ title: "sql", path: "/noteBook/sql/index.md" }],
-        },
-        {
-          title: "git",
-          path: "/noteBook/git/",
-          collapsable: false, // 不折叠
-          // children: [{ title: "git", path: "/noteBook/git/index.md" }],
-        },
-        {
-          title: "算法",
-          path: "/noteBook/algrithum/",
-          collapsable: false, // 不折叠
-          // children: [{ title: "算法", path: "/noteBook/algrithum/index.md" }],
-        },
-        {
-          title: "运维",
-          path: "/noteBook/operate/",
-          collapsable: false, // 不折叠
-          // children: [{ title: "运维", path: "/noteBook/operate/index.md" }],
-        },
-        {
-          title: "源码阅读",
-          path: "/noteBook/code_reading/",
-          collapsable: false, // 不折叠
-          children: [
-            // { title: "源码阅读", path: "/noteBook/code_reading/index.md" },
-          ],
-        },
-        {
-          title: "工作经验",
-          path: "/noteBook/experience/",
-          collapsable: false, // 不折叠
-          children: [
-            // {
-            //   title: "登录",
-            //   path: "/noteBook/experience/login.md",
-            // },
-          ],
-        },
-        {
-          title: "优秀资源",
-          path: "/noteBook/resources/",
-          collapsable: false, // 不折叠
-          children: [
-            // { title: "优秀资源", path: "/noteBook/resources/index.md" },
-          ],
-        },
-        {
-          title: "其他",
-          path: "/noteBook/others/",
-          collapsable: false, // 不折叠
-          // children: [{ title: "其他", path: "/noteBook/others/index.md" }],
-        },
-      ],
       "/withoutCode/": [
         {
           title: "生活不止代码",
@@ -120,7 +29,97 @@ module.exports = {
           collapsable: true, // 不折叠
         },
       ],
-      "/": [""],
+      "/": [
+        {
+          title: "站点介绍",
+          path: "/",
+          collapsable: true, // 不折叠
+          children: [],
+        },
+        {
+          title: "设计",
+          path: "/design/",
+          collapsable: false, // 不折叠
+          sidebarDepth: 5,
+          children: [],
+        },
+        {
+          title: "前端",
+          path: "/frontEnd/",
+          collapsable: false, // 不折叠
+          // children: [{ title: "前端", path: "/frontEnd/index.md" }],
+        },
+        {
+          title: "后端",
+          path: "/backEnd/",
+          collapsable: false, // 不折叠
+          // 多级嵌套看这里的目录结构
+          children: [
+            {
+              title: "java",
+              path: "/backEnd/java/",
+              children: [{ title: "api速记", path: "/backEnd/java/api/" }],
+            },
+          ],
+        },
+        {
+          title: "sql",
+          path: "/sql/",
+          collapsable: false, // 不折叠
+          // children: [{ title: "sql", path: "/sql/index.md" }],
+        },
+        {
+          title: "git",
+          path: "/git/",
+          collapsable: false, // 不折叠
+          // children: [{ title: "git", path: "/git/holes/" }],
+        },
+        {
+          title: "算法",
+          path: "/algrithum/",
+          collapsable: false, // 不折叠
+          // children: [{ title: "算法", path: "/algrithum/index.md" }],
+        },
+        {
+          title: "运维",
+          path: "/operate/",
+          collapsable: false, // 不折叠
+          // children: [{ title: "运维", path: "/operate/index.md" }],
+        },
+        {
+          title: "源码阅读",
+          path: "/code_reading/",
+          collapsable: false, // 不折叠
+          children: [
+            // { title: "源码阅读", path: "/code_reading/index.md" },
+          ],
+        },
+        {
+          title: "工作经验",
+          path: "/experience/",
+          collapsable: false, // 不折叠
+          children: [
+            // {
+            //   title: "通用登录",
+            //   path: "/experience/common_login/",
+            // },
+          ],
+        },
+        {
+          title: "优秀资源",
+          path: "/resources/",
+          collapsable: false, // 不折叠
+          children: [
+            // { title: "优秀资源", path: "/resources/index.md" },
+          ],
+        },
+        {
+          title: "其他",
+          path: "/others/",
+          collapsable: false, // 不折叠
+          // children: [{ title: "其他", path: "/others/index.md" }],
+        },
+      ],
     },
   },
 };
